513c914a4d0c558fe300f937b867358e48f5ca57
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-16 22:56:32 +0200

    Update output_&_math.go

--- output_&_math.go
+++ output_&_math.go
@@ -9,9 +9,20 @@ func main(){
 
 
 // print command in GO
+
 package main
 import "fmt"
 
 func main() {
 	fmt.Print(12)
+}
+
+
+// "I Love GO" program
+
+package main
+import "fmt"
+
+func main() {
+    fmt.Print("I Love GO") 
 }
